5e2ce6ce6cff2f63c5fa8e1dbab9a0d1491fbfe6
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-10-29 10:29:54 +0000

    Add pedestal interaction, lantern moves from pedestal to player and vice-versa

--- Assets/Scripts/Goncalo/Interactables/Pedestal.cs
+++ Assets/Scripts/Goncalo/Interactables/Pedestal.cs
@@ -1,16 +1,56 @@
+using System.Collections;
 using UnityEngine;
 
-public class Pedestal : MonoBehaviour
+public class Pedestal : MonoBehaviour, IInteractable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private GameObject interactedText;
+    [SerializeField]
+    private GameObject lantern;
+    [SerializeField]
+    private GameObject pedestalSpot;
+    [SerializeField]
+    private GameObject playerSpot;
+
+    private bool hasLantern;
+
+    private void Start()
     {
-        
+        hasLantern = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (hasLantern)
+        {
+            //insert code to transfer energy to lantern
+        }
+    }
+
+    public void Interact()
+    {
+        if (hasLantern)
+        {
+            lantern.transform.parent = playerSpot.transform;
+            lantern.transform.localPosition = Vector3.zero;
+            hasLantern = false;
+        }
+        else if (!hasLantern)
+        {
+            lantern.transform.parent = pedestalSpot.transform;
+            lantern.transform.localPosition = Vector3.zero;
+            hasLantern = true;
+        }
+
+        StartCoroutine(InteractedCoroutine());
+    }
+
+    private IEnumerator InteractedCoroutine()
+    {
+        //Activates a text indicating that the cube was interacted with
+        //Changes which portal is active
+        interactedText.SetActive(true);
+        yield return new WaitForSeconds(.5f);
+        interactedText.SetActive(false);
     }
 }
