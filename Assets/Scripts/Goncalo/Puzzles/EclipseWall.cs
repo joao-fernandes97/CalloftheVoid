05719e447821d314d0bcf46ea88fb4d180ab2c57
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-12-17 15:44:27 +0000

    Eclipse walls now slowly disappear and reappear

--- Assets/Scripts/Goncalo/Puzzles/EclipseWall.cs
+++ Assets/Scripts/Goncalo/Puzzles/EclipseWall.cs
@@ -5,10 +5,63 @@ public class EclipseWall : MonoBehaviour
     [SerializeField]
     private GameObject wallModel;
     [SerializeField]
+    private GameObject jupiterSymbol;
+    [SerializeField]
+    private GameObject saturnSymbol;
+    [SerializeField]
     private Portal portal;
     [SerializeField]
     private PlanetsEnum portalPlanet;
     private PlanetsEnum portalPlanetActive;
+
+    [SerializeField]
+    private float timeToDisappear = 1f;
+    private bool disappearing = false;
+
+    private Color materialColor;
+    private Color jupiterColor;
+    private Color saturnColor;
+    private float alpha = 1f;
+
+    private void Start()
+    {
+        materialColor = wallModel.GetComponent<Renderer>().material.color;
+        jupiterColor = jupiterSymbol.GetComponent<Renderer>().material.color;
+        saturnColor = saturnSymbol.GetComponent<Renderer>().material.color;
+    }
+
+    private void FixedUpdate()
+    {
+        if (disappearing)
+        {
+            alpha -= Time.fixedDeltaTime/ timeToDisappear;
+            if (alpha <= 0f)
+            {
+                alpha = 0f;
+                wallModel.SetActive(false);
+            }
+        }
+        else
+        {
+            alpha += Time.fixedDeltaTime / timeToDisappear;
+            if (alpha > 1f)
+            {
+                alpha = 1f;
+            }
+        }
+
+        if (alpha > 0f)
+        {
+            wallModel.SetActive(true);
+        }
+
+        materialColor = new Color(materialColor.r, materialColor.g, materialColor.b, alpha);
+        jupiterColor = new Color(jupiterColor.r, jupiterColor.g, jupiterColor.b, alpha);
+        saturnColor = new Color(saturnColor.r, saturnColor.g, saturnColor.b, alpha);
+        wallModel.GetComponent<Renderer>().material.color = materialColor;
+        jupiterSymbol.GetComponent<Renderer>().material.color = materialColor;
+        saturnSymbol.GetComponent<Renderer>().material.color = materialColor;
+    }
 
 
     private void OnTriggerStay(Collider other)
@@ -23,11 +76,13 @@ public class EclipseWall : MonoBehaviour
             if (lantern.Planet == portalPlanet && lantern.emitingEnergy)
             {
                 Debug.Log("detected neptune");
-                wallModel.SetActive(false);
+                //wallModel.SetActive(false);
+                disappearing = true;
             }
-            else if (lantern.Planet == portalPlanet && !lantern.emitingEnergy)
+            else if (lantern.Planet == portalPlanet && !lantern.emitingEnergy && portalPlanetActive != portalPlanet)
             {
-                wallModel.SetActive(true);
+                //wallModel.SetActive(true);
+                disappearing = false;
             }
         }
     }
@@ -37,6 +92,7 @@ public class EclipseWall : MonoBehaviour
         if (other.GetComponent<Lantern>() != null && portalPlanetActive != portalPlanet)
         {
             wallModel.SetActive(true);
+            disappearing = false;
         }
     }
 
@@ -50,11 +106,13 @@ public class EclipseWall : MonoBehaviour
 
         if (portalPlanetActive == portalPlanet)
         {
-            wallModel.SetActive(false);
+            //wallModel.SetActive(false);
+            disappearing = true;
         }
         else
         {
-            wallModel.SetActive(true);
+            //wallModel.SetActive(true);
+            disappearing = false;
         }
     }
 
